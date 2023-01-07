4536d8f21814b8400eb5ae292581951c5861b9af
Ivan <ivan.savkiv.igorovych@gmail.com>
2023-01-07 19:53:29 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Container } from "@mui/system";
+import MenuIcon from "@mui/icons-material/Menu";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar position="fixed">
+      <Container fixed>
+        <Toolbar>
+          <Box mr={3}>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Typography variant="h6" className={classes.title}>
+            React MaterialUI site
+          </Typography>
+          <Box mr={3}>
+            <Button color="inherit" variant="outlined">
+              Log In
+            </Button>
+          </Box>
+          <Button color="secondary" variant="contained">
+            Sign Up
+          </Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
